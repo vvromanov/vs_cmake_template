73d5e37ffa2bf8077f9d294503c7c69d84e95a2f
Vladimir Romanov <vromanov@gmail.com>
2025-01-15 18:04:20 +0100

    Initial commit

---
+++ vs_cmake_template.cpp
@@ -0,0 +1,12 @@
+﻿// vs_cmake_template.cpp : Defines the entry point for the application.
+//
+
+#include "vs_cmake_template.h"
+
+using namespace std;
+
+int main()
+{
+	cout << "Hello CMake." << endl;
+	return 0;
+}

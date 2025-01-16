4edf958d396831fac8509b57ec48c62fa406d915
Vladimir Romanov <vromanov@gmail.com>
2025-01-16 08:11:26 +0100

    Change license, remove header

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # vs_cmake_template
+Template for simple test projects
+* Visual Studio
+* CMake
+* Google test
+* WSL
+* vcpkg
--- src/vs_cmake_template.cpp
+++ src/vs_cmake_template.cpp
@@ -1,13 +1,8 @@
-﻿// vs_cmake_template.cpp : Defines the entry point for the application.
-//
-
-#include "vs_cmake_template.h"
+﻿#include <iostream>
 #include "vs_cmake_template_lib.h"
-
-using namespace std;
 
 int main()
 {
-	cout << "1+2=" << Add(1,2) << endl;
+	std::cout << "1+2=" << Add(1,2) << std::endl;
 	return 0;
 }
